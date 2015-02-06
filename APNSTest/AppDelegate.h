b0a6f5cd759eaa6506e871b5ea58c16b07833c99
Seema Patil <patil_s@Seemas-Mac-mini.local>
2015-02-06 18:21:20 +0530

    Initial Commit

---
+++ APNSTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APNSTest
+//
+//  Created by Seema Patil on 2/6/15.
+//  Copyright (c) 2015 Seema Patil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APNSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APNSTest
+//
+//  Created by Seema Patil on 2/6/15.
+//  Copyright (c) 2015 Seema Patil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
